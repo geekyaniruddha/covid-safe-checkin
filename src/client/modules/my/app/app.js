36ca86decc505a78a288e834e67e7b0e7e3b5e7d
Aniruddha Basak <aniruddha.abk@gmail.com>
2022-03-28 23:51:44 +0530

    Designed the form

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+const defaultData = {
+    Name: '',
+    Email: '',
+    Mobile: ''
+}
+export default class App extends LightningElement {
+    formData = defaultData; 
+    formchange(event){
+        const {name, value} = event.target;
+        this.formData = {...this.formData, [name]:value};
+    }
+    checkInHandler(event){
+        event.preventDefault();
+        console.log(this.formData);
 
-export default class App extends LightningElement {}
+    }
+}
