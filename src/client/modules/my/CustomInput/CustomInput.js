2f6c2a68756ea39fccbe97a4b48f75447bda6e73
Aniruddha Basak <aniruddha.abk@gmail.com>
2022-03-29 09:13:06 +0530

    Built reusable component and used it

---
+++ src/client/modules/my/CustomInput/CustomInput.js
@@ -0,0 +1,17 @@
+import { LightningElement, api } from 'lwc';
+export default class CustomInput extends LightningElement {
+    @api name = 'field';
+    @api type = 'text';
+    @api required = false;
+    @api label = 'Form Label';
+
+    get uniqueId(){
+        return this.name;
+    }
+
+    inputHandler(event){
+        const{name, value} = event.target;
+        console.log({name, value});
+    }
+
+}
