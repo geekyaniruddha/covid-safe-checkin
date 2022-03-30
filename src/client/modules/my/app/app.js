ae8ac6960003be5b703aac1b1476b04c68a3d73f
Aniruddha Basak <aniruddha.abk@gmail.com>
2022-03-31 00:04:19 +0530

    Created api to post the records from front end to google sheet

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -4,6 +4,7 @@ const defaultData = {
     Email: '',
     Mobile: ''
 }
+const BASE_URL = 'http://localhost:3002';
 export default class App extends LightningElement {
     formData = defaultData; 
     formchange(event){
@@ -12,7 +13,29 @@ export default class App extends LightningElement {
     }
     checkInHandler(event){
         event.preventDefault();
+        this.formData = {...this.formData, 
+            "Date":new Date().toLocaleDateString(),
+            "Time":new Date().toLocaleTimeString()
+        }
         console.log(this.formData);
-
+        fetch(`${BASE_URL}/api/v1/submit`, {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(this.formData)
+        }).then(response=>response.json())
+          .then(result=>console.log(result))
+          .catch(error=>console.error(error))
+    }
+    // connectedCallback is given by lwc which gets called on the load of the page
+    connectedCallback(){
+        this.fetchData();
+    }
+    fetchData(){
+        fetch(`${BASE_URL}/api/v1/sheetname`)
+        .then(response=>response.json()) //response comes in binary string that's why I have converted it to json
+        .then(result=>console.log(result))
+        .catch(error=>console.log(error))
     }
 }
